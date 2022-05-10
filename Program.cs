4cbb9482017e255baa1da2aeac8dc8856f7c0aec
bibi100101 <gabriel.radosz@gmail.com>
2022-05-10 23:25:03 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Nowy_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+    }
+
+    class Game
+    {
+        Player playerA;
+        Player playerB;
+
+        private int randomNumber = Random.Range(0, 100);
+
+        Game(string nameA, string nameB)
+        {
+            playerA.Name = nameA;
+            playerB.Name = nameB;
+        }
+    }
+
+    public struct Player
+    {
+        string name;
+        int guess;
+    }
+}
