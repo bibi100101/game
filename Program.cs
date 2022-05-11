b05ad8156beb3ad951e4713d23a2c2cd35eb39a4
bibi100101 <gabriel.radosz@gmail.com>
2022-05-11 22:13:34 +0200

    a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nowy_folder
 {
@@ -18,19 +20,63 @@ namespace Nowy_folder
 
     public class Game
     {
+        //tworzy graczy i liste odpowiedzi
         Player playerA;
         Player playerB;
-        
-        static Random rand = new Random();
-        private int randomNumber = rand.Next(0, 100);
 
+        List<int> guesses = new List<int>();
+        
+        
+        
         public Game(string nameA, string nameB)
         {
+            //daje imię graczom
             playerA.name = nameA;
             playerB.name = nameB;
+
+            //gra w nieskończoność
+            while (true)
+            {
+               Play(); 
+            }
+        }
+
+        //*Głowna gra
+        public void Play()
+        {
+            //*tworzy odpowiedź
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 100);
+            
+            //Bierze odpowiedzi graczy i dodaje je do listy
+            Console.WriteLine($"{playerA.name}, podaj liczbe");
+            playerA.guess = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{playerB.name}, podaj liczbe");
+            playerB.guess = Convert.ToInt32(Console.ReadLine());
+
+            guesses.Add(playerA.guess);
+            guesses.Add(playerB.guess);
+            
+            //!sprawdza jaka odpowiedź jest najbliższa do prawidłowej odpowiedzi
+            int closest = guesses.Aggregate((x, y) => Math.Abs(x - randomNumber) < Math.Abs(y - randomNumber) ? x : y);
+            
+            //pisze prawodłową odpowiedź
+            Console.WriteLine($"Prawidlowa odpowiedz to: {randomNumber}");
+            
+            //pisze kto wygrał
+            if (playerA.guess == closest)
+            {
+                Console.WriteLine($"{playerA.name} Wygral!");
+            }
+            else if (playerB.guess == closest)
+            {
+                Console.WriteLine($"{playerB.name} Wygral");
+            }
         }
     }
 
+    //!struktura gracza
     public struct Player
     {
         public string name;
